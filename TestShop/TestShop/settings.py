c1cf6074093b362761793088f8060f4892293c33
BotrRobotgnom <user84443@gmail.com>
2023-05-16 14:04:31 +0300

    Update settings.py

--- TestShop/TestShop/settings.py
+++ TestShop/TestShop/settings.py
@@ -24,14 +24,14 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'intense-gorge-59251.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
