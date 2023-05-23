fe9309881abb5bfb0162cc2297dffbf493c471fe
BotrRobotgnom <user84443@gmail.com>
2023-05-23 04:28:23 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
+
 
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
