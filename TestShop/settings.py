143de622a6b607a30800f6a4bb11269215b8951e
BotrRobotgnom <user84443@gmail.com>
2023-05-30 03:43:56 +0300

    Update settings.py

--- TestShop/settings.py
+++ TestShop/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
