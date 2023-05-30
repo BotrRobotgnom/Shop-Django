3ae5e8cdbff7890febf745ab2b94d5d516f4db8c
BotrRobotgnom <user84443@gmail.com>
2023-05-30 04:09:50 +0300

    fix admin + fix del product from cart

--- main/admin.py
+++ main/admin.py
@@ -11,12 +11,11 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'get_items_display', 'get_total_price')
 
     def get_items_display(self, obj):
-        items = obj.items.all()
-        return ', '.join([f'{item.name}: {item.quantity}' for item in items])
+        return ', '.join([f'{product.name}: {quantity}' for product, quantity in self.items.items()])
     get_items_display.short_description = 'Items'
 
     def get_total_price(self, obj):
-        return obj.total_price
+        return obj.get_total_price()
     get_total_price.short_description = 'Total Price'
 
 admin.site.register(Order, OrderAdmin)
